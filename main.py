357e03847417a73f3e5fef125dc04715d128e478
kapis18 <kapis1881@gmail.com>
2020-03-21 21:16:51 +0100

    Added the option of loading pictures from disk

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import sys
 
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QPushButton, QVBoxLayout
-from PyQt5.QtGui import QIcon, QPixmap, QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QPushButton, QFileDialog
+from PyQt5.QtGui import QPixmap, QPainter, QColor
 
 
 class App(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.label = QLabel(self)
+        self.label.resize(550, 500)
+        self.setImageToLabel('res/empty.png')
         self.title = 'CatSadify'
         self.width = 800
         self.height = 600
@@ -19,21 +22,19 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setFixedSize(self.width, self.height)
         self.setAutoFillBackground(True)
+
         palette = self.palette()
         palette.setColor(self.backgroundRole(), QColor(56, 56, 61, 127))
         self.setPalette(palette)
 
         gridLayout = QGridLayout()
 
-        label = QLabel(self)
-        label.resize(550, 500)
-        pixmap = QPixmap('res/hurtingcat.jpg')
-        label.setPixmap(pixmap.scaled(label.size(), QtCore.Qt.IgnoreAspectRatio))
+        loadButton = QPushButton("Load image");
+        loadButton.clicked.connect(lambda: self.openImage())
 
-        gridLayout.addWidget(label, 0, 0)
-        gridLayout.addWidget(QPushButton("Load image"), 1, 0, 1, 1, Qt.AlignCenter)
+        gridLayout.addWidget(self.label, 0, 0)
+        gridLayout.addWidget(loadButton, 1, 0, 1, 1, Qt.AlignCenter)
         gridLayout.addWidget(QLabel("test"), 0, 1, Qt.AlignCenter)
-
         self.setLayout(gridLayout)
         self.show()
 
@@ -41,6 +42,14 @@ class App(QWidget):
         painter = QPainter(self)
         painter.setPen(Qt.white)
         painter.drawRect(10, 10, 552, 502)
+
+    def openImage(self):
+        imagePath, _ = QFileDialog.getOpenFileName()
+        self.setImageToLabel(imagePath)
+
+    def setImageToLabel(self, imagePath):
+        pixmap = QPixmap(imagePath)
+        self.label.setPixmap(pixmap.scaled(self.label.size(), QtCore.Qt.IgnoreAspectRatio))
 
 
 if __name__ == '__main__':
