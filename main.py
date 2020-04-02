a9aa599f1beed3553861bdfa9697c8d158e18dda
kapis18 <kapis1881@gmail.com>
2020-04-02 02:03:24 +0200

    Added template overlaying, first semi-working version alas still buggy

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ class App(QWidget):
         self.cat_ext_cascade = cv2.CascadeClassifier('res/haarcascade_frontalcatface_extended.xml')
         self.scale_factor = 1.05
         self.neighbours = 3
+        self.rect_width = 0
+        self.rect_height = 0
+        self.rect_x = 0
+        self.rect_y = 0
         self.slider_scale_factor = QSlider(Qt.Horizontal)
         self.slider_neighbours = QSlider(Qt.Horizontal)
         self.param_visibility = False
@@ -50,14 +54,13 @@ class App(QWidget):
         vbox = QVBoxLayout()
         self.groupbox.setLayout(vbox)
 
-        #setup slider for parameter 1
+        # setup slider for parameter 1
         vbox.addWidget(QLabel("Scale factor"))
         vbox.addWidget(self.slider_scale_factor)
         self.slider_scale_factor.setMinimum(105)
         self.slider_scale_factor.setMaximum(200)
         self.slider_scale_factor.setValue(150)
         self.slider_scale_factor.setTickInterval(10)
-
 
         # setup slider for parameter 2
         vbox.addWidget(QLabel("Neighbours"))
@@ -67,14 +70,13 @@ class App(QWidget):
         self.slider_neighbours.setValue(5)
         self.slider_neighbours.setTickInterval(9)
 
-        #connect sliders to corresponding methods
+        # connect sliders to corresponding methods
         self.slider_scale_factor.valueChanged.connect(self.valuechange_scalefactor)
         self.slider_neighbours.valueChanged.connect(self.valuechange_neighbours)
 
         self.groupbox.setVisible(self.param_visibility)
 
         # button for loading images to GUI
-
         load_button = QPushButton("Load image")
         load_button.clicked.connect(lambda: self.openImage())
 
@@ -82,10 +84,15 @@ class App(QWidget):
         process_button = QPushButton("Find face")
         process_button.clicked.connect(lambda: self.processImage(self.current_path))
 
+        # sadify button
+        sadify_button = QPushButton("SADIFY")
+        sadify_button.clicked.connect(lambda: self.blend_images())
+
         # setting up elements on the layout
         grid_layout.addWidget(self.label, 0, 0)
         grid_layout.addWidget(load_button, 1, 0, 1, 1, Qt.AlignCenter)
         grid_layout.addWidget(process_button, 1, 1, 1, 1, Qt.AlignCenter)
+        grid_layout.addWidget(sadify_button, 1, 2, 1, 1, Qt.AlignCenter)
         grid_layout.addWidget(self.groupbox, 0, 1, Qt.AlignCenter)
 
         self.setLayout(grid_layout)
@@ -98,7 +105,6 @@ class App(QWidget):
 
     def openImage(self):
         image_path, _ = QFileDialog.getOpenFileName()
-        print(image_path)
         if image_path == "":
             return
         self.current_file = os.path.basename(image_path)
@@ -127,13 +133,18 @@ class App(QWidget):
                                                          minNeighbors=self.neighbours)
         for (x, y, w, h) in cats:
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        for (x, y, w, h) in cats_ext:
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # for (x, y, w, h) in cats_ext:
+        #     img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        self.rect_x = cats.item(0)
+        self.rect_y = cats.item(1)
+        self.rect_width = cats.item(2)
+        self.rect_height = cats.item(3)
 
         self.setImageToLabelFromImage(img)
 
     def valuechange_scalefactor(self):
-        value = self.slider_scale_factor.value()/100
+        value = self.slider_scale_factor.value() / 100
         print(value)
         self.scale_factor = value
 
@@ -141,6 +152,33 @@ class App(QWidget):
         value = self.slider_neighbours.value()
         print(value)
         self.neighbours = value
+
+    def overlay_image_alpha(self, img, img_overlay, pos, alpha_mask):
+        x, y = pos
+
+        y1, y2 = max(0, y), min(img.shape[0], y + img_overlay.shape[0])
+        x1, x2 = max(0, x), min(img.shape[1], x + img_overlay.shape[1])
+
+        y1o, y2o = max(0, -y), min(img_overlay.shape[0], img.shape[0] - y)
+        x1o, x2o = max(0, -x), min(img_overlay.shape[1], img.shape[1] - x)
+
+        if y1 >= y2 or x1 >= x2 or y1o >= y2o or x1o >= x2o:
+            return
+
+        channels = img.shape[2]
+
+        alpha = alpha_mask[y1o:y2o, x1o:x2o]
+        alpha_inv = 1.0 - alpha
+
+        for c in range(channels):
+            img[y1:y2, x1:x2, c] = (alpha * img_overlay[y1o:y2o, x1o:x2o, c] +
+                                    alpha_inv * img[y1:y2, x1:x2, c])
+    def blend_images(self):
+        img = cv2.imread(self.current_path)
+        template = cv2.imread("res/templates/template.png", -1)
+        template = cv2.resize(template, (self.rect_width, self.rect_height))
+        self.overlay_image_alpha(img, template[:, :, 0:3], (self.rect_x, self.rect_y), template[:, :, 3] / 255.0)
+        self.setImageToLabelFromImage(img)
 
 
 if __name__ == '__main__':
